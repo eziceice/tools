6c1ac0d3509105166b4eb76054bf2f205704f81b
yutian.li <Elementli1>
2019-12-16 15:33:42 +1100

    format file.

--- bngenerator.py
+++ bngenerator.py
@@ -2,7 +2,8 @@ from argparse import ArgumentParser
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-c', '--category', help='branch or investigation or feature', default='feature')
+    parser.add_argument(
+        '-c', '--category', help='branch or investigation or feature', default='feature')
     parser.add_argument('-j', '--jira', help='jira ticket number', )
     parser.add_argument('-d', '--description', help='jira ticket description')
 
